75f8b4482aaba5cc30cf060d826b8569c10a75b6
DESKTOP-NJUC59J\Antonio <agtespecialista@gmail.com>
2019-04-07 23:30:47 -0500

    first commit

---
+++ src/main/java/com/spring5/crud/Crud001Application.java
@@ -0,0 +1,13 @@
+package com.spring5.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud001Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring5/crud/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring5.crud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Crud001Application.class);
+	}
+
+}
